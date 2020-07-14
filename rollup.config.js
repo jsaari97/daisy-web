638309a000e2382e5d305a3b32041d08c70d06e2
Jim Saari <jim.saari97@gmail.com>
2020-07-14 23:12:15 +0300

    Add deployment scripts

--- rollup.config.js
+++ rollup.config.js
@@ -40,7 +40,7 @@ export default {
     // https://github.com/rollup/plugins/tree/master/packages/commonjs
     resolve({
       browser: true,
-      dedupe: ["svelte", "jszip"],
+      dedupe: ["svelte", "jszip", "xpath"],
     }),
     commonjs(),
 
