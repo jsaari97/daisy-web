b4c3c1873d59d6e826535a06a12d895724db9b14
Jim Saari <jim.saari97@gmail.com>
2020-07-14 23:12:53 +0300

    Updates

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+var cacheName = "daisyweb-cache-" + Date.now();
+var filesToCache = [
+   "/daisy-web/",
+   "/daisy-web/index.html",
+   "/daisy-web/global.css",
+   "/daisy-web/build/bundle.js",
+   "/daisy-web/build/bundle.css",
+  "https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap",
+];
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (thisCacheName) {
+          if (thisCacheName !== cacheName) {
+            return caches.delete(thisCacheName);
+          }
+        })
+      );
+    })
+  );
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async function () {
+      const response = await caches.match(e.request);
+      return response || fetch(e.request);
+    })()
+  );
+});
